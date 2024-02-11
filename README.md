e7637fd2d8f09a9aad3bf0f8896a809727cd3e56
Mitchell Barry <mitch.barry@gmail.com>
2024-02-10 18:56:48 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# Home Maintenance Log
+
+Self-hosted web app for logging maintenance tasks around the house.
+
+* [Screenshots](#screenshots)
+* [Getting Started](#getting-started)
+* [Features](#features)
+* [Technical Things](#technical-things)
+* [Possible Features](#possible-features)
+* [Development](#development)
+* [License](#license)
+
+## Screenshots
+
+_To do..._
+
+## Getting Started
+
+To quickstart with Docker,
+
+```bash
+docker run -d -p 8080:80 ghcr.io/mitch-b/maintenance-log:latest
+```
+
+Then open your browser to `http://localhost:8080`.
+
+To run with a sidecar SQL database container, use the provided [`docker-compose.yml`](./docker-compose.yml) file.
+
+## Features
+
+* Add a new log entry
+  * Task, datetime, description, duration, notes ...
+  * Can log parts/products used
+* Set reminders for future maintenance (?)
+
+## Technical Things
+
+* .NET 8 Blazor Web App (SSR+WASM rendering)
+* Entity Framework Core
+* SQL Server (or other)
+* Containers
+* GitHub Actions (to CI/CD container images)
+
+## Possible Features
+
+* Expose as sensors to Home Assistant (?) via MQTT (?)
+* Multiple homes
+* SSO/OAuth
+* Webhooks for events
+
+## Development
+
+If you're a GitHub user, try in Codespaces!
+
+Otherwise, for local development:
+
+* [.NET 8 SDK](https://dotnet.microsoft.com/download/dotnet/8.0)
+* Your favorite IDE, or [Visual Studio Code](https://code.visualstudio.com/)
+* (_Optional_) [Docker](https://www.docker.com/products/docker-desktop)
+
+```bash
+cd src/
+docker build -t maintenancelog:local .
+```
+
+## Local Development
+
+### Secrets Management
+
+```bash
+cd src/MaintenanceLog
+# dotnet tool install --global dotnet-ef
+dotnet ef database update
+
+dotnet user-secrets set EmailConfig:SmtpUser myuser
+dotnet user-secrets set EmailConfig:SmtpPass mypass
+dotnet user-secrets set EmailConfig:SmtpHost my-smtp.host.com
+dotnet user-secrets set EmailConfig:SmtpPort 587
+```
+
+## License
+
+See [LICENSE](./LICENSE)
---
+++ src/MaintenanceLog/Program.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MaintenanceLog.Client.Pages;
+using MaintenanceLog.Components;
+using MaintenanceLog.Components.Account;
+using MaintenanceLog.Data;
+using MaintenanceLog.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
+
+var connectionString = builder.Configuration.GetConnectionString("MaintenanceLogDb") ?? throw new InvalidOperationException("Connection string 'MaintenanceLogDb' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+builder.Services
+    .AddFluentEmail(builder.Configuration.GetValue<string>("EmailConfig:SmtpFrom"))
+    .AddSmtpSender(
+        builder.Configuration.GetValue<string?>("EmailConfig:SmtpHost"), 
+        builder.Configuration.GetValue<int?>("EmailConfig:SmtpPort") ?? 587,
+        builder.Configuration.GetValue<string?>("EmailConfig:SmtpUser"),
+        builder.Configuration.GetValue<string?>("EmailConfig:SmtpPass"));
+//builder.Services.AddTransient<IEmailSender<ApplicationUser>, EmailSender>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(MaintenanceLog.Client._Imports).Assembly);
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalIdentityEndpoints();
+
+app.Run();
