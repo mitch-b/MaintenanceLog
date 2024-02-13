fa82cad203876a09abee4850dfe48b2378cd2fd8
Mitchell Barry <mitch.barry@gmail.com>
2024-02-13 04:52:55 +0000

    Add task to kill open ports that don't auto close

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,2 +1,3 @@
 dotnet tool install --global dotnet-ef
 dotnet dev-certs https --trust
+chmod +x ./.scripts/free-ports.sh 
---
+++ .scripts/free-ports.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Array of ports to check
+ports=(5217 7019)
+
+# Loop over the ports
+for port in "${ports[@]}"
+do
+  # Use lsof to find processes using the port and awk to get the PID
+  echo "Checking port $port"
+  pid=$(lsof -t -i:$port)
+
+  # If a process is using the port, kill it
+  if [ -n "$pid" ]; then
+    echo "Killing process $pid on port $port"
+    kill -9 $pid
+  else
+    echo "No process using port $port"
+  fi
+done
