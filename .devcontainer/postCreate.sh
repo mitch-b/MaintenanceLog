f94ff954f556c06980f0445de8a541ebb15168d4
Mitchell <mitch.barry@gmail.com>
2024-02-18 20:09:49 +0000

    try setting path in postCreate

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,4 +1,9 @@
 dotnet dev-certs https --trust
 #chmod +x ./.scripts/*
 
+set +e
 dotnet tool install --global dotnet-ef
+set -e
+
+echo "Setting .dotnet tool path"
+export PATH="$PATH:/$USER/.dotnet/tools"
