567fdc4404f1b032e2837c9b14383c3a5c30c44d
Mitchell Barry <mitch.barry@gmail.com>
2024-03-30 16:37:01 +0000

    Create client service for suggestions

---
+++ src/MaintenanceLog.Client/Services/HttpSmartTaskDefinitionStepService.cs
@@ -0,0 +1,27 @@
+﻿using MaintenanceLog.Common.Contracts;
+using MaintenanceLog.Common.Models.Requests;
+using MaintenanceLog.Common.Models.Responses;
+using MaintenanceLog.Data.Entities;
+using MaintenanceLog.Data.Services.Client;
+
+namespace MaintenanceLog.Client.Services
+{
+    public class HttpSmartTaskDefinitionStepService(JsonHttpClient httpClient) : ISmartTaskDefinitionStepService
+    {
+        private readonly JsonHttpClient _httpClient = httpClient;
+
+        public async Task<List<TaskDefinitionStep>?> SuggestStepsForTaskDefinition(string? itemName, string? description = null, List<TaskDefinitionStep>? taskDefinitionSteps = null)        
+        {
+            var response = await _httpClient.PostAsJsonAsync<SuggestTaskDefinitionStepsRequest, SuggestTaskDefinitionStepsResponse>("api/task-definitions/estimate-task-definition-steps", 
+                new SuggestTaskDefinitionStepsRequest
+                {
+                    ItemName = itemName,
+                    Description = description,
+                    TaskDefinitionSteps = taskDefinitionSteps
+                });
+            return response is null 
+                ? throw new Exception("API did not return the estimated Cron schedule") 
+                : response.TaskDefinitionSteps;
+        }
+    }
+}
